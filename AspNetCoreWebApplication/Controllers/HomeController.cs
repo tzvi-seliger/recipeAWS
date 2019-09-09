88315982ffda4fc8a38ee82b2a275d258655645a
unknown <tzviseliger@gmail.com>
2019-09-09 01:23:59 -0400

    Saving

--- AspNetCoreWebApplication/Controllers/HomeController.cs
+++ AspNetCoreWebApplication/Controllers/HomeController.cs
@@ -15,13 +15,13 @@ namespace AspNetCoreWebApplication.Controllers
         {
             string connString;
             connString =
-                @"Data Source=database-1.cur7afppexfe.us-east-2.rds.amazonaws.com; Initial Catalog=Recipes;User ID=admin;Password=Whatthe770!";
+                @"Data Source=database-1.cur7afppexfe.us-east-2.rds.amazonaws.com; Initial Catalog=rdsadmin;User ID=admin;Password=Whatthe770!";
             List<Recipe> recipes = new List<Recipe>();
             using (SqlConnection conn =
                    new SqlConnection(connString))
             {
-                SqlCommand command =
-                    new SqlCommand("select * from recipes", conn);
+           /*     SqlCommand command =
+                    new SqlCommand("select * from recipes", conn);*/
                 conn.Open();
 
 
