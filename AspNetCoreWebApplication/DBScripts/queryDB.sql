0425ac272a932bd4eb8ebca4da0c53929a955820
unknown <tzviseliger@gmail.com>
2019-09-10 16:41:06 -0400

    saving:

---
+++ AspNetCoreWebApplication/DBScripts/queryDB.sql
@@ -0,0 +1,12 @@
+﻿SELECT * FROM recipes;
+
+--Display all instructions with corresponding Recipes
+SELECT * FROM Instructions JOIN Recipes ON Instructions.RecipeId = Recipes.RecipeId
+
+--Display all instructions with corresponding Recipe for 'Blueberry Muffin'
+SELECT * FROM Instructions JOIN Recipes ON Instructions.RecipeId = Recipes.RecipeId
+WHERE Recipes.RecipeName = 'Blueberry Muffin'
+
+--Display all instructions with corresponding Recipe for 'ChocolateChip Cookies'
+SELECT * FROM Instructions JOIN Recipes ON Instructions.RecipeId = Recipes.RecipeId
+WHERE Recipes.RecipeName = 'ChocolateChip Cookies'
---
+++ AspNetCoreWebApplication/DBScripts/seedDB.sql
@@ -0,0 +1,31 @@
+﻿use RecipeManager;
+
+insert into Recipes(RecipeName) VALUES 
+	('Blueberry Muffin'),
+	('ChocolateChip Cookies'),
+	('Classic Pancakes'),
+	('Tiramisu')
+insert into Ingredients(IngredientName) VALUES 
+	('All Purpose Flour'),
+	('Eggs'),
+	('Vanilla Extract'),
+	('Whole Milk')
+
+insert into Instructions(RecipeId, InstructionContent)
+SELECT Recipes.RecipeId, 'This is my First Instruction'
+FROM Recipes Where RecipeName = 'Blueberry Muffin'
+
+insert into Instructions(RecipeId, InstructionContent)
+SELECT Recipes.RecipeId, 'This is my Second Instruction'
+FROM Recipes Where RecipeName = 'Blueberry Muffin'
+
+insert into Instructions(RecipeId, InstructionContent)
+SELECT Recipes.RecipeId, 'This is my First Instruction'
+FROM Recipes Where RecipeName = 'ChocolateChip Cookies'
+
+insert into Instructions(RecipeId, InstructionContent)
+SELECT Recipes.RecipeId, 'This is my Second Instruction'
+FROM Recipes Where RecipeName = 'ChocolateChip Cookies'
+
+
+
