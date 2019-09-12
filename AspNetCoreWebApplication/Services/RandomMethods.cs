5a06cd59d4cebebd6220e121060f0d8dbab6c2b7
unknown <tzviseliger@gmail.com>
2019-09-11 20:39:37 -0400

    Saving

--- AspNetCoreWebApplication/Services/RandomMethods.cs
+++ AspNetCoreWebApplication/Services/RandomMethods.cs
@@ -7,9 +7,10 @@ namespace AspNetCoreWebApplication.Services
 {
     public static class RandomMethods
     {
-        public static string ToTitle(string str)
-        {
-            return String.Join(' ', str.Split(' ').Select(x => $"{x.Substring(0, 1).ToUpper()}{x.Substring(1).ToLower()}"));
-        }
+        public static string ToTitle(string str) => String.Join(' ',
+                                                                str.Split(' ')
+                                                                   .Select(x => $"{x.Substring(0, 1).ToUpper()}{x.Substring(1).ToLower()}"));
+
+
     }
 }
